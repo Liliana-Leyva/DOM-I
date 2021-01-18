3ff6d294c2a824aa6736c5845c8bb52c9cba8515
Liliana Leyva <ingeleyva@yahoo.com>
2021-01-17 21:31:28 -0600

    MVP

--- js/index.js
+++ js/index.js
@@ -41,4 +41,71 @@ const siteContent = {
 let logo = document.getElementById("logo-img");
 logo.setAttribute('src', siteContent["nav"]["img-src"])
 
-//
+//document.querySelector('body').appendChild(logo)
+
+//navigation Bar
+const navBar = document.querySelectorAll("nav a");
+ navBar.forEach((item,i) => {
+   item.textContent = siteContent["nav"][`nav-item-${i + 1}`]
+ });
+
+ //adding h1, button and logo cta
+ const h1Cta = document.querySelector('h1')
+ h1Cta.textContent= siteContent["cta"]["h1"];
+
+ const buttonCta= document.querySelector('button')
+ buttonCta.textContent = siteContent["cta"]["button"]
+
+ const imgFirst = document.getElementById("cta-img");
+ imgFirst.setAttribute('src', siteContent["cta"]["img-src"])
+ 
+
+
+ //adding main content
+ const h4OneTop = document.querySelector(".main-content .top-content .text-content:nth-child(1) h4");
+ h4OneTop.textContent = siteContent["main-content"]["features-h4"];
+
+ const h4TwoTop = document.querySelector(".main-content .top-content .text-content:nth-child(2) h4");
+ h4TwoTop.textContent = siteContent["main-content"]["about-h4"];
+ 
+ const pOneTop = document.querySelector(".main-content .top-content .text-content:nth-child(1) p");
+ pOneTop.textContent = siteContent["main-content"]["features-content"];
+ 
+ const pTwoTop = document.querySelector(".main-content .top-content .text-content:nth-child(2) p");
+ pTwoTop.textContent = siteContent["main-content"]["about-content"];
+
+ const middleImg = document.querySelector(".main-content .middle-img")
+middleImg.setAttribute('src',siteContent["main-content"]["middle-img-src"])
+ 
+
+//adding bottom content
+//h4
+const h4OneBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(1) h4");
+ h4OneBottom.textContent = siteContent["main-content"]["services-h4"];
+ const h4TwoBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(2) h4");
+ h4TwoBottom.textContent = siteContent["main-content"]["product-h4"];
+ const h4ThreeBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(3) h4");
+ h4ThreeBottom.textContent = siteContent["main-content"]["vision-h4"];
+//P
+
+ const pOneBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(1) p");
+ pOneBottom.textContent = siteContent["main-content"]["services-content"];
+
+ const pTwoBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(2) p");
+ pTwoBottom.textContent = siteContent["main-content"]["product-content"];
+
+ const pThreeBottom = document.querySelector(".main-content .bottom-content .text-content:nth-child(3) p");
+ pThreeBottom.textContent = siteContent["main-content"]["vision-content"];
+
+
+ //contact information
+
+ const h4Contact = document.querySelector(".contact h4");
+ h4Contact.textContent = siteContent["contact"]["contact-h4"]
+
+//  const p1Contact = document.querySelector(".contact:nth-child(1) p");
+//  p1Contact.textContent = siteContent["contact"]["address"];
+
+//  const p2Contact = document.querySelector(".contact:nth-child(2) p");
+//  p2Contact.textContent = siteContent["contact"]["email"];
+
